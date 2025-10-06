e38926567ae1bb5bcd042d384b6f146f89567fc7
anirisaihan <jessica.anirisaihan@mail.utoronto.ca>
2025-10-06 16:31:40 -0400

    Task 3a Complete

--- src/main/java/usecase/GetAverageGradeUseCase.java
+++ src/main/java/usecase/GetAverageGradeUseCase.java
@@ -29,6 +29,14 @@ public final class GetAverageGradeUseCase {
         // TODO Task 3a: Complete the logic of calculating the average course grade for
         //              your team members. Hint: the getGrades method might be useful.
 
+        for (String member : team.getMembers()) {
+            var grades = gradeDataBase.getGrade(member,course);
+            if (grades != null) {
+                sum += grades.getGrade();
+            }
+            count++;
+        }
+
         if (count == 0) {
             return 0;
         }
