70a078e1fcaf8b392e2ca050beec22d5e79e2b62
Colour1205 <me@uaena.io>
2025-10-06 12:04:45 -0400

    implemented task 3a

--- src/main/java/usecase/GetAverageGradeUseCase.java
+++ src/main/java/usecase/GetAverageGradeUseCase.java
@@ -28,6 +28,15 @@ public final class GetAverageGradeUseCase {
         // Call the API to get all the grades for the course for all your team members
         // TODO Task 3a: Complete the logic of calculating the average course grade for
         //              your team members. Hint: the getGrades method might be useful.
+        for (String member : team.getMembers()){
+            Grade[] grades = gradeDataBase.getGrades(member);
+            for (Grade grade : grades){
+                sum += grade.getGrade();
+            }
+            count += grades.length;
+        }
+
+
 
         if (count == 0) {
             return 0;
