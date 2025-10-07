04ef8c1522f34ad6fb02e63c2236900832ea6981
Caleb Irwin <me@calebirwin.ca>
2025-10-07 11:41:55 -0400

    imp getToken

--- src/main/java/example/GetToken.java
+++ src/main/java/example/GetToken.java
@@ -1,5 +1,7 @@
 package example;
 
+import java.io.IOException;
+
 /**
  * Class for getting your Grade API token.
  */
@@ -7,15 +9,22 @@ public class GetToken {
 
     /**
      * Run to get your token after you complete the code.
+     * 
      * @param args unused arguments
      */
     public static void main(String[] args) {
         GetExample example = new GetExample();
 
-        String gradeAPISignUpURL = "https://grade-apis.panchen.ca/signUp";
-        // TODO Task 1: use the run method to get your Grade API token (or obtain your token through a web browser).
+        String gradeAPISignUpURL = "https://grade-apis.panchen.ca/signUp?username=cal0";
+        // TODO Task 1: use the run method to get your Grade API token (or obtain your
+        // token through a web browser).
         String response = "";
-        // response = example.run( ...)
+        try {
+            response = example.run(gradeAPISignUpURL);
+        } catch (IOException e) {
+            System.err.println("Error requesting token: " + e.getMessage());
+            return;
+        }
         System.out.println(response);
     }
 }
